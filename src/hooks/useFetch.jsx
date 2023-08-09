e501f9a638ac630bf2b6f3ae003e7c48e0fda63b
kostiantyn <kostiantyn@test.com>
2023-08-09 11:55:38 -0230

    +useFetch hook

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (dataURL, dataOptions) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchError, setFetchError] = useState(null);
+
+  useEffect(() => {
+    let isMounted = true;
+    const fetchData = async (url, options) => {
+      setIsLoading(true);
+      try {
+        const request = await fetch(url, options);
+        const response = await request.json();
+        if (isMounted) {
+          setData(response);
+          setFetchError(null);
+        }
+      } catch ({ name, message }) {
+        if (isMounted) {
+          setData([]);
+          setFetchError({ name, message });
+        }
+      } finally {
+        isMounted && setIsLoading(false);
+      }
+    };
+
+    fetchData(dataURL, dataOptions);
+
+    const cleanUp = () => {
+      console.log("cleaned up");
+      isMounted = false;
+    };
+
+    return cleanUp;
+  }, [dataURL, dataOptions]);
+
+  return { data, isLoading, fetchError };
+};
+
+export default useFetch;
