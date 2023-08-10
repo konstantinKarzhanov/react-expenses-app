2ef319146da5f88403cc507667a28349fb910776
kostiantyn <kostiantyn@test.com>
2023-08-10 11:43:00 -0230

    removed commented code

--- src/components/ExpensesList.jsx
+++ src/components/ExpensesList.jsx
@@ -4,13 +4,9 @@ import ExpensesListItem from "./ExpensesListItem";
 
 const ExpensesList = () => {
   const { dataFromAPI } = useContext(Context);
-  // const listArr = dataFromAPI.map(({ id, ...data }) => (
-  //   <ExpensesListItem key={id} dataHandle={data} />
-  // ));
   const listArr = dataFromAPI
     .reduce((acc, { expenses }) => acc.concat(expenses), [])
     .map((item) => <ExpensesListItem key={item.id} dataHandle={item} />);
-  console.log(listArr);
   return <ul>{listArr}</ul>;
 };
 
