6169f156b2d6f03af44584ba0774012c45483f6d
kostiantyn <kostiantyn@test.com>
2023-08-12 16:34:58 -0230

    +handleClick, +processDelete, +processEdit
    This functions was added for actions with btn--edit and btn--delete

--- src/components/ExpensesListItem.jsx
+++ src/components/ExpensesListItem.jsx
@@ -1,11 +1,29 @@
 import React, { useRef } from "react";
+import useMainContext from "../hooks/useMainContext";
 import ExpensesDetailsList from "./ExpensesDetailsList";
 import ExpensesButton from "./ExpensesButton";
 
 const ExpensesListItem = ({ idHandle, dataHandle }) => {
+  const { EXPENSES_URL, deleteItem } = useMainContext();
   const editBtn = useRef();
   const deleteBtn = useRef();
-  const handleClick = () => {};
+
+  const processEdit = () => {};
+  const processDelete = (id) => {
+    deleteItem(EXPENSES_URL, id);
+    console.log(`item with id: ${id} has been deleted`);
+  };
+
+  const handleClick = (event) => {
+    const target = event.target.closest("button");
+    if (!target) return;
+    const parentContainer = target.parentElement;
+    const dataId = +parentContainer.previousElementSibling.id
+      .split("-")
+      .slice(-1)[0];
+    target === editBtn.current && processEdit();
+    target === deleteBtn.current && processDelete(dataId);
+  };
 
   return (
     <li>
