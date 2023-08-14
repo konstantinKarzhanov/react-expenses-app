a1a18277c99cd0e726185701aadf42cea03a4b6e
BestProductsAlways <127046853+BestProductsAlways@users.noreply.github.com>
2023-08-14 15:36:52 -0230

    re-updated files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+// styles
+import "./App.css";
 
+import { Route, Routes } from "react-router-dom";
+
+import AboutPage from './pages/AboutPage';
+import CategoriesPage from "./pages/CategoriesPage";
+import { ExpensesContextProvider } from "./context/ExpensesContext";
+// pages
+import ExpensesPage from "./pages/ExpensesPage";
+import InsightsPage from "./pages/InsightsPage";
+import { MainContextProvider } from "./context/MainContext";
+import NotFoundPage from "./pages/NotFoundPage";
+import Overlay from "./components/Overlay";
+import React from "react";
 // hooks
 import useOverlayContext from "./hooks/useOverlayContext";
 
 // components
-import { MainContextProvider } from "./context/MainContext";
-import { ExpensesContextProvider } from "./context/ExpensesContext";
-import Overlay from "./components/Overlay";
-
-// pages
-import ExpensesPage from "./pages/ExpensesPage";
-import NotFoundPage from "./pages/NotFoundPage";
-import AboutPage from './pages/AboutPage';
-import InsightsPage from "./pages/InsightsPage";
-
-// styles
-import "./App.css";
 
 function App() {
   const { isOverlay } = useOverlayContext();
@@ -31,7 +31,8 @@ function App() {
           <Route path="insights" element={<InsightsPage />}></Route>
           <Route path="about" element ={<AboutPage />}></Route>
           <Route path="*" element={<NotFoundPage />}></Route>
-        </Routes>
+          
+          <Route path="categories" element={<CategoriesPage />}></Route>       </Routes>
       </ExpensesContextProvider>
     </MainContextProvider>
   );
