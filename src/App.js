b06d150ce612ee6081e2acc8cb86486c1557c665
kostiantyn <kostiantyn@test.com>
2023-08-15 11:11:43 -0230

    Page structure changes, 'Footer', '#header-text'
    removed 'Footer' from all pages and put it to 'App.js', minor structure changes in 'AboutPage' and 'CategoriesPage', removed '#header-text' from 'about.css' and 'Insights.css' and put it as class to 'App.css' so it can be used for each page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { MainContextProvider } from "./context/MainContext";
 import { ExpensesContextProvider } from "./context/ExpensesContext";
 import Overlay from "./components/Overlay";
 import Button from "./components/Button";
+import Footer from "./components/Footer";
 
 // pages
 import ExpensesPage from "./pages/ExpensesPage";
@@ -21,28 +22,30 @@ import InsightsPage from "./pages/InsightsPage";
 import "./App.css";
 
 function App() {
-
   const { isOverlay, handleClick } = useOverlayContext();
 
   return (
-    <MainContextProvider>
-      <ExpensesContextProvider>
-        {isOverlay && <Overlay />}
-        <Routes>
-          <Route path="/" element={<ExpensesPage />}></Route>
-          <Route path="expenses" element={<ExpensesPage />}></Route>
-          <Route path="categories" element={<CategoriesPage />}></Route>
-          <Route path="insights" element={<InsightsPage />}></Route>
-          <Route path="about" element={<AboutPage />}></Route>
-          <Route path="*" element={<NotFoundPage />}></Route>
-        </Routes>
-        <Button
-          handleClick={handleClick}
-          classHandle="btn--menu"
-          children="menu"
-        />
-      </ExpensesContextProvider>
-    </MainContextProvider>
+    <>
+      <MainContextProvider>
+        <ExpensesContextProvider>
+          {isOverlay && <Overlay />}
+          <Routes>
+            <Route path="/" element={<ExpensesPage />}></Route>
+            <Route path="expenses" element={<ExpensesPage />}></Route>
+            <Route path="categories" element={<CategoriesPage />}></Route>
+            <Route path="insights" element={<InsightsPage />}></Route>
+            <Route path="about" element={<AboutPage />}></Route>
+            <Route path="*" element={<NotFoundPage />}></Route>
+          </Routes>
+          <Button
+            handleClick={handleClick}
+            classHandle="btn--menu"
+            children="menu"
+          />
+        </ExpensesContextProvider>
+      </MainContextProvider>
+      <Footer />
+    </>
   );
 }
 
