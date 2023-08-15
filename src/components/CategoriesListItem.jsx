f93d5adedb5269685f21893226fe5c04740677b4
kostiantyn <kostiantyn@test.com>
2023-08-14 22:18:22 -0230

    +CategoriesListItem, +code to deleteItem inside MainContext for CategoriesListItem
    renamed CategoryListIteme to CategoriesListItem

---
+++ src/components/CategoriesListItem.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import useMainContext from "../hooks/useMainContext";
+import Button from "./Button";
+
+const CategoriesListItem = ({ idHandle, children }) => {
+  const { CATEGORIES_URL, deleteItem } = useMainContext();
+
+  const processDelete = (id) => {
+    if (id === 1) {
+      alert("Sorry you cannot delete this category");
+    } else if (window.confirm("Are you sure you want to delete this item?")) {
+      deleteItem(CATEGORIES_URL, id);
+      console.log(`item with id: ${id} has been deleted`);
+    }
+  };
+
+  const handleClick = () => {
+    processDelete(idHandle);
+  };
+
+  return (
+    <li id={`category-${idHandle}`}>
+      <p>{children}</p>
+      <Button
+        handleClick={handleClick}
+        classHandle={"btn--delete"}
+        children={"delete"}
+      />
+    </li>
+  );
+};
+
+export default CategoriesListItem;
