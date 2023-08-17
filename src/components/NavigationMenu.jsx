84a68e6de5fe5e46f144f7582314f94a8831101f
kostiantyn <kostiantyn@test.com>
2023-08-17 00:18:27 -0230

    +hide Overlay if NavLink was clicked

--- src/components/NavigationMenu.jsx
+++ src/components/NavigationMenu.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import useOverlayContext from "../hooks/useOverlayContext";
 import "../styles/navigation.css";
 
 const NavigationMenu = () => {
+  const { handleClick } = useOverlayContext();
   const navMenuArr = ["expenses", "categories", "insights", "about"];
   const navList = navMenuArr.map((item, index) => (
     <li key={index} className="pad">
-      <NavLink to={item} className="d-block">
+      <NavLink onClick={handleClick} to={item} className="d-block">
         {item}
       </NavLink>
     </li>
