e55d46255337f9c29ea17cdbd10cb75d5642b192
kostiantyn <kostiantyn@test.com>
2023-08-17 02:13:30 -0230

    increased margin-bottom for 'MainTitle'

--- src/components/MainTitle.jsx
+++ src/components/MainTitle.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const MainTitle = ({ children }) => {
-  return <h1 className="mb--1 fs--3xl fancy--text">{children}</h1>;
+  return <h1 className="mb--3 fs--3xl fancy--text">{children}</h1>;
 };
 
 export default MainTitle;
